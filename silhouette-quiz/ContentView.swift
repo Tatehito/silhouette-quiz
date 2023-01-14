2785e03d5561b943f7045a9500d18fca5ad91063
Kento Sato <tatehito.st@gmail.com>
2023-01-14 20:26:00 +0900

    ImagePickerのサンプル

--- silhouette-quiz/ContentView.swift
+++ silhouette-quiz/ContentView.swift
@@ -1,26 +1,31 @@
-//
-//  ContentView.swift
-//  silhouette-quiz
-//
-//  Created by Kento Sato on 2023/01/14.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: UIImage?
+    @State var showingImagePicker = false
+        
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let uiImage = image {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+            } else {
+                Image("noimage")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+            }
+            Spacer().frame(height: 32)
+            Button(action: {
+                showingImagePicker = true
+            }) {
+                Text("フォトライブラリから選択")
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
+        }
     }
 }
