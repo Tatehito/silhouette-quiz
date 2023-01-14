beda34cfe52a46c185453243be2af4ceee4e2fd9
Kento Sato <tatehito.st@gmail.com>
2023-01-14 20:20:24 +0900

    Create Xcode Project

---
+++ silhouette-quiz/silhouette_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  silhouette_quizApp.swift
+//  silhouette-quiz
+//
+//  Created by Kento Sato on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct silhouette_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
