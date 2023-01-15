6a4fc3a18061fe555e008f63cd8f1bd8ac672e38
Kento Sato <tatehito.st@gmail.com>
2023-01-15 14:13:44 +0900

    クイズ一覧画面を作成

---
+++ silhouette-quiz/QuizListView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct QuizListView: View {
+    @State private var image: UIImage?
+        
+    var body: some View {
+        VStack {
+            Text("ここはクイズ一覧画面です。")
+        }
+    }
+}
--- silhouette-quiz/silhouette_quizApp.swift
+++ silhouette-quiz/silhouette_quizApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct silhouette_quizApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuizListView()
         }
     }
 }
