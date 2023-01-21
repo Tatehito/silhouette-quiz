11d49ee3bbac37d458029fb8c57cd91a0642acae
Kento Sato <tatehito.st@gmail.com>
2023-01-21 15:00:24 +0900

    クイズ登録画面で写真を選択できる

--- silhouette-quiz/QuizCreateView.swift
+++ silhouette-quiz/QuizCreateView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
+import PhotosUI
 
 struct QuizCreateView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var showingImagePicker = false
     @State private var title = ""
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
 
     var body: some View {
         VStack {
@@ -17,6 +21,21 @@ struct QuizCreateView: View {
                 _ = Quiz.create(title: title)
                 dismiss()
             }
+            Button(action: {
+                showingImagePicker = true
+            }) {
+                Text("もんだいの写真を選択")
+            }
+            image?.resizable().scaledToFit()
+        }.sheet(isPresented: $showingImagePicker) {
+            SwiftUIPicker(image: $inputImage)
+        }.onChange(of: inputImage) { newValue in
+            loadImage()
         }
+    }
+    
+    func loadImage(){
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
     }
 }
