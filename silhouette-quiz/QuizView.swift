abbefe95795862253b7161c555ba44b999e92057
Kento Sato <tatehito.st@gmail.com>
2023-02-08 17:41:19 +0900

    クイズ画面のUI調整

--- silhouette-quiz/QuizView.swift
+++ silhouette-quiz/QuizView.swift
@@ -6,22 +6,51 @@ struct QuizView: View {
     
     var quizList: [Quiz]
     @State private var quizIndex: Int = 0
+    @State private var quizTitle: String?
     @State private var displayImage: UIImage?
     @State private var isQuestionMode: Bool = true
     
     var body: some View {
         VStack {
             if let uiImage = displayImage {
-                Image(uiImage: uiImage).resizable().scaledToFit()
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
             }
             if (isQuestionMode) {
-                Button("こたえをみる") {
+                Text("だーれだ？")
+                    .font(.system(size: 30, weight: .black))
+                    .padding(.top, 20)
+                    .padding(.bottom, 100)
+                Button(action: {
                     handleClickShowAnswerImage()
+                }){
+                    Text("こたえをみる")
+                        .bold()
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(25)
                 }
+                .padding(.bottom, 20)
             } else {
-                Button("つぎのもんだい") {
+                Text(quizTitle!+"！")
+                    .font(.system(size: 30, weight: .black))
+                    .padding(.top, 20)
+                    .padding(.bottom, 100)
+                Button(action: {
                     handleClickNextQuestion()
+                }){
+                    Text("つぎのもんだい")
+                        .bold()
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(25)
                 }
+                .padding(.bottom, 20)
             }
             Button("クイズをおわる") {
                 dismiss()
@@ -30,11 +59,13 @@ struct QuizView: View {
         .navigationBarBackButtonHidden(true)
         .onAppear {
             self.displayImage = quizList[quizIndex].questionImage
+            self.quizTitle = quizList[quizIndex].title
         }
     }
     
     func handleClickShowAnswerImage() {
         self.displayImage = quizList[quizIndex].answerImage
+        self.quizTitle = quizList[quizIndex].title
         self.isQuestionMode.toggle()
     }
     
@@ -46,6 +77,7 @@ struct QuizView: View {
             self.quizIndex = quizIndex + 1
         }
         self.displayImage = quizList[quizIndex].questionImage
+        self.quizTitle = quizList[quizIndex].title
         self.isQuestionMode.toggle()
     }
 }
