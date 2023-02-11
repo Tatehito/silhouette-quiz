2e7bd8b9bf6ae1b9f8a0921acc1dd22e81abd550
Kento Sato <tatehito.st@gmail.com>
2023-02-11 22:18:06 +0900

    キーボード表示時に全体が上に迫り上がるのを防止する

--- silhouette-quiz/QuizEditView.swift
+++ silhouette-quiz/QuizEditView.swift
@@ -14,88 +14,90 @@ struct QuizEditView: View {
     let realm = try! Realm()
     
     var body: some View {
-        VStack {
-            Text("クイズのなまえ")
-                .frame(maxWidth: .infinity, alignment: .leading)
-            TextField("クイズのなまえをいれてください。", text: $quiz.title)
-                .keyboardType(.default)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Spacer()
-
-            HStack {
-                Text("こたえ")
-                Button("しゃしんをえらぶ") {
-                    handleClickSelectAnswerImageButton()
-                }.frame(maxWidth: .infinity, alignment: .trailing)
-            }.frame(maxWidth: .infinity, alignment: .leading)
+        GeometryReader { _ in
             VStack {
-                if let uiImage = answerUIImage {
-                    Image(uiImage: uiImage).resizable().scaledToFit()
-                } else {
-                    Image(uiImage: quiz.answerImage).resizable().scaledToFit()
-                }
-            }.frame(height: 200)
+                Text("クイズのなまえ")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                TextField("クイズのなまえをいれてください。", text: $quiz.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Spacer()
 
-            HStack {
-                Text("もんだい")
-                Button("しゃしんをえらぶ") {
-                    handleClickSelectQuestionImageButton()
-                }.frame(maxWidth: .infinity, alignment: .trailing)
-            }.frame(maxWidth: .infinity, alignment: .leading)
-            VStack {
-                if let uiImage = questionUIImage {
-                    Image(uiImage: uiImage).resizable().scaledToFit()
-                } else {
-                    Image(uiImage: quiz.questionImage).resizable().scaledToFit()
-                }
-            }.frame(height: 200)
-            
-            Spacer()
-            
-            Button(action: {
-                handleClickSubmitButton()
-            }){
-                Text("ほぞんする")
-                    .bold()
-                    .padding()
-                    .frame(width: 200, height: 50)
-                    .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(25)
-            }
-            // 上寄せにする
-            Spacer()
-            
-        }.sheet(isPresented: $showingQuestionImagePicker) {
-            SwiftUIPicker(image: $questionUIImage)
+                HStack {
+                    Text("こたえ")
+                    Button("しゃしんをえらぶ") {
+                        handleClickSelectAnswerImageButton()
+                    }.frame(maxWidth: .infinity, alignment: .trailing)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                VStack {
+                    if let uiImage = answerUIImage {
+                        Image(uiImage: uiImage).resizable().scaledToFit()
+                    } else {
+                        Image(uiImage: quiz.answerImage).resizable().scaledToFit()
+                    }
+                }.frame(height: 200)
 
-        }.sheet(isPresented: $showingAnswerImagePicker) {
-            SwiftUIPicker(image: $answerUIImage)
-
-        }.onChange(of: answerUIImage) { newImage in
-            if answerUIImage == nil {
-                return
-            }
-            questionUIImage = answerUIImage?.silhouetteImageGenerate()
-        }
-        // navigationBarBackButtonのカスタマイズ
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
+                HStack {
+                    Text("もんだい")
+                    Button("しゃしんをえらぶ") {
+                        handleClickSelectQuestionImageButton()
+                    }.frame(maxWidth: .infinity, alignment: .trailing)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                VStack {
+                    if let uiImage = questionUIImage {
+                        Image(uiImage: uiImage).resizable().scaledToFit()
+                    } else {
+                        Image(uiImage: quiz.questionImage).resizable().scaledToFit()
+                    }
+                }.frame(height: 200)
+                
+                Spacer()
+                
                 Button(action: {
-                    dismiss()
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.backward")
-                            .font(.system(size: 17, weight: .medium))
-                        Text("もどる")
+                    handleClickSubmitButton()
+                }){
+                    Text("ほぞんする")
+                        .bold()
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(25)
+                }
+                // 上寄せにする
+                Spacer()
+                
+            }.sheet(isPresented: $showingQuestionImagePicker) {
+                SwiftUIPicker(image: $questionUIImage)
+
+            }.sheet(isPresented: $showingAnswerImagePicker) {
+                SwiftUIPicker(image: $answerUIImage)
+
+            }.onChange(of: answerUIImage) { newImage in
+                if answerUIImage == nil {
+                    return
+                }
+                questionUIImage = answerUIImage?.silhouetteImageGenerate()
+            }
+            // navigationBarBackButtonのカスタマイズ
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.backward")
+                                .font(.system(size: 17, weight: .medium))
+                            Text("もどる")
+                        }
                     }
                 }
             }
-        }
-        .padding(.horizontal, 20)
-        .padding(.top, 30)
+            .padding(.horizontal, 20)
+            .padding(.top, 30)
+        // キーボード表示時に全体が上に迫り上がるのを防止する
+        }.ignoresSafeArea(.keyboard, edges: .bottom)
     }
     
     private func handleClickSelectQuestionImageButton() {
